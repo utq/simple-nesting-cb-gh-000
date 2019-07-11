8a80a44b2e26d94e19593182db93cbc5cf2a7bdb
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 15:32:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -123,6 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+		 programmer_hash[:dennis_ritchie][:languages] = ["Smalltalk", "LISP", "Assembly"]
 
 end
