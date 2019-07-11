e8603ae53b14e236af89cc91da5463fefb579e6c
Celal Cankaya <celalcankaya@outlook.com>
2019-07-11 15:03:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+		 programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
@@ -80,7 +82,7 @@ def adding_matz
         }
      }
 
-
+		 programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
 end
 
 def changing_alan
@@ -101,7 +103,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
-
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 end
 
@@ -121,5 +123,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 
 end
